4279350ee2b839bf75fbaace8ab9946d20bd77d9
weiraneve <907221539@qq.com>
2024-02-23 21:32:54 +0800

    feat: add key_down for click

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,25 @@ use std::time::Duration;
 use enigo::*;
 
 fn main() {
-    thread::sleep(Duration::from_secs(2));
+    thread::sleep(Duration::from_secs(3));
 
     let mut enigo = Enigo::new();
-    enigo.key_click(Key::Layout('v'));
+    enigo.key_down(Key::Control);
+    enigo.key_click(Key::Layout('m'));
+    enigo.key_up(Key::Control);
+
+    thread::sleep(Duration::from_secs(1));
+    enigo.key_down(Key::Control);
+    enigo.key_click(Key::Layout('g'));
+    enigo.key_up(Key::Control);
+
+    thread::sleep(Duration::from_secs(1));
+    enigo.key_down(Key::Control);
+    enigo.key_click(Key::Layout('p'));
+    enigo.key_up(Key::Control);
+
+    thread::sleep(Duration::from_secs(1));
+    enigo.key_down(Key::Control);
+    enigo.key_click(Key::Layout('m'));
+    enigo.key_up(Key::Control);
 }
