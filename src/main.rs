fe40669509e5004fe6468d4d4f52ab3562b536a4
weiraneve <907221539@qq.com>
2024-02-23 21:04:24 +0800

    feat: add enigo dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::thread;
+use std::time::Duration;
+
+use enigo::*;
+
 fn main() {
-    println!("Hello, world!");
+    thread::sleep(Duration::from_secs(2));
+
+    let mut enigo = Enigo::new();
+    enigo.key_click(Key::Layout('v'));
 }
